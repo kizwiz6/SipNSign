bc624a17ec106a040787263cd5ea4d9a2fa5e936
Kieran Emery <kizwiz@hotmail.co.uk>
2024-12-13 13:54:24 +0000

    UI update: Update logo, colours, etc.

---
+++ SipNSign/Services/IThemeService.cs
@@ -0,0 +1,32 @@
+﻿using com.kizwiz.sipnsign.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.kizwiz.sipnsign.Services
+{
+    /// <summary>
+    /// Defines the contract for theme management
+    /// </summary>
+    public interface IThemeService
+    {
+        /// <summary>
+        /// Gets the current theme colors
+        /// </summary>
+        ThemeColors CurrentTheme { get; }
+
+        /// <summary>
+        /// Sets the application theme
+        /// </summary>
+        /// <param name="theme">The theme to apply</param>
+        void SetTheme(CustomAppTheme theme);
+
+        /// <summary>
+        /// Gets the current theme setting
+        /// </summary>
+        /// <returns>The current AppTheme</returns>
+        CustomAppTheme GetCurrentTheme();
+    }
+}
