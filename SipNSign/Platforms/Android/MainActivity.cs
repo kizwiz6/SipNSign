a91b6da913ce2d3fef739fbc6265baaa4e57e983
Kieran Emery <kizwiz@hotmail.co.uk>
2024-12-21 18:25:54 +0000

    Splash screen changes updated.

--- SipNSign/Platforms/Android/MainActivity.cs
+++ SipNSign/Platforms/Android/MainActivity.cs
@@ -1,18 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
 
-namespace com.kizwiz.sipnsign
+namespace com.kizwiz.sipnsign;
+
+[Activity(Theme = "@style/Maui.SplashTheme",
+          MainLauncher = true,
+          ConfigurationChanges = ConfigChanges.ScreenSize |
+                                ConfigChanges.Orientation |
+                                ConfigChanges.UiMode |
+                                ConfigChanges.ScreenLayout |
+                                ConfigChanges.SmallestScreenSize |
+                                ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
+    protected override void OnCreate(Bundle savedInstanceState)
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            Window?.SetStatusBarColor(Android.Graphics.Color.ParseColor("#1a237e"));
-        }
+        base.OnCreate(savedInstanceState);
+        Window?.SetStatusBarColor(Android.Graphics.Color.ParseColor("#1a237e"));
     }
 }
