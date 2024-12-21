15e19cd5d4c3e901e188647f395ff04ae4be6073
Kieran Emery <kizwiz@hotmail.co.uk>
2024-12-21 14:43:46 +0000

    SplashScreen implementation.

--- SipNSign/Platforms/Android/MainActivity.cs
+++ SipNSign/Platforms/Android/MainActivity.cs
@@ -9,5 +9,10 @@ namespace com.kizwiz.sipnsign
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Window?.SetStatusBarColor(Android.Graphics.Color.ParseColor("#1a237e"));
+        }
     }
 }
